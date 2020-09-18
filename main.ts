7cbae349ac2412121515ec905ed75386cd9f6216
Aryan Gajelli <45264359+aryanGajelli@users.noreply.github.com>
2020-09-18 13:50:05 -0400

    Test Commit 1

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-input.buttonA.onEvent(ButtonEvent.Click, function () {
-    light.showRing(
-    `red red red red red black black black black black`
-    )
-    music.jumpUp.play()
-})
 input.buttonB.onEvent(ButtonEvent.Click, function () {
     light.showRing(
     `black black black black black red red red red red`
