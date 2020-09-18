b7505470049592d86cd88a0e3c88d7c78eb99a3a
Aryan Gajelli <45264359+aryanGajelli@users.noreply.github.com>
2020-09-18 17:35:05 -0400

    Cooler looking pew pew

--- main.ts
+++ main.ts
@@ -12,12 +12,12 @@ input.buttonA.onEvent(ButtonEvent.Click, function () {
     music.jumpUp.play()
     control.runInParallel(function () {
         light.setBrightness(10)
-        for(let i =4; i>=0; i--){        
-            light.setPixelColor(i, jump_colors[4-i]);
-            light.setPixelColor(9-i, jump_colors[4-i]);
-            pause(75)
+        for(let i =0; i<5; i++){     
+            light.setPixelColor(4-i, jump_colors[i]);
+            light.setPixelColor(i+5, jump_colors[i]);
+            pause(50)
         }
-pause(125)
+        pause(125)
         light.clear()
     })
 })
@@ -28,23 +28,52 @@ input.onGesture(Gesture.TiltRight, function () {
 input.buttonB.onEvent(ButtonEvent.Click, function () {
     keyboard.key("i", KeyboardKeyEvent.Press)
     keyboard.clearAllKeys()
+    music.stopAllSounds()
     music.pewPew.play()
+   
     control.runInParallel(function () {
         light.setBrightness(10)
-        for(let j =4; j>=0; j--){        
-            light.setPixelColor(j, shoot_colors[4-j]);
-            light.setPixelColor(9-j, shoot_colors[4-j]);
-            pause(75)
+        for(let i =0 ; i<3; i++){     
+            light.setPixelColor(2-i, shoot_colors[i]);
+            light.setPixelColor(2+i, shoot_colors[i]);
+            pause(20)
         }
-pause(125)
+        for(let i =2 ; i>=0; i--){     
+            light.setPixelColor(7-i, shoot_colors[4-i]);
+            light.setPixelColor(7+i, shoot_colors[4-i]);
+            pause(20)
+        }
+
+        for(let i =0 ; i<3; i++){     
+            light.setPixelColor(2-i, 0x00000000);
+            light.setPixelColor(2+i, 0x00000000);
+            pause(20)
+        }
+        for(let i =2 ; i>=0; i--){     
+            light.setPixelColor(7-i, 0x00000000);
+            light.setPixelColor(7+i, 0x00000000);
+            pause(20)
+        }
         light.clear()
     })
 })
 input.onSwitchMoved(SwitchDirection.Left, function () {
     keyboard.key("p", KeyboardKeyEvent.Press)
 })
-let jump_colors = [light.rgb(3, 4, 94), light.rgb(0, 119, 182), light.rgb(0, 180, 216), light.rgb(80, 187, 227), light.rgb(202, 240, 248)]
-let shoot_colors = [light.rgb(157, 2, 8), light.rgb(208, 0, 0), light.rgb(232, 93, 4), light.rgb(244, 140, 6), light.rgb(255, 186, 8)]
+let jump_colors = [    
+    light.rgb(3, 4, 94),
+    light.rgb(0, 119, 182),
+    light.rgb(0, 180, 216),  
+    light.rgb(80, 187, 227),
+    light.rgb(202, 240, 248)   
+]
+let shoot_colors = [
+    light.rgb(157, 2, 8),
+    light.rgb(220, 47, 2),
+    light.rgb(232, 93, 4),
+    light.rgb(244, 140, 6),
+    light.rgb(255, 186, 8)
+]
 forever(function () {
     if (input.buttonsAB.isPressed()) {
         keyboard.key("j", KeyboardKeyEvent.Press)
