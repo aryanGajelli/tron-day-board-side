f01814e102e4d99f9f6853ff454da021ee68db88
Aryan Gajelli <45264359+aryanGajelli@users.noreply.github.com>
2020-09-18 16:41:43 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,32 +1,95 @@
 def on_switch_moved_right():
     keyboard.key("[", KeyboardKeyEvent.PRESS)
+    keyboard.clear_all_keys()
 input.on_switch_moved(SwitchDirection.RIGHT, on_switch_moved_right)
 
 def on_gesture_tilt_left():
     keyboard.key("a", KeyboardKeyEvent.PRESS)
+    keyboard.clear_all_keys()
 input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
 
 def on_button_a_click():
     keyboard.key("q", KeyboardKeyEvent.PRESS)
+    keyboard.clear_all_keys()
     music.jump_up.play()
-    light.show_ring("""yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow""")
-    light.clear()
+    
+    def on_run_in_parallel():
+        light.set_brightness(10)
+        light.set_pixel_color(4, light.rgb(3, 4, 94))
+        pause(75)
+        light.set_brightness(10)
+        light.set_pixel_color(3, light.rgb(0, 119, 182))
+        pause(75)
+        light.set_brightness(10)
+        light.set_pixel_color(2, light.rgb(72, 191, 227))
+        pause(75)
+        light.set_brightness(10)
+        light.set_pixel_color(1, light.rgb(0, 180, 216))
+        pause(75)
+        light.set_brightness(10)
+        light.set_pixel_color(0, light.rgb(128, 255, 219))
+        pause(200)
+        light.clear()
+    control.run_in_parallel(on_run_in_parallel)
+    
+    
+    def on_run_in_parallel2():
+        light.set_pixel_color(5, light.rgb(3, 4, 94))
+        pause(75)
+        light.set_pixel_color(6, light.rgb(0, 119, 182))
+        pause(75)
+        light.set_pixel_color(7, light.rgb(0, 180, 216))
+        pause(75)
+        light.set_pixel_color(8, light.rgb(100, 223, 223))
+        pause(75)
+        light.set_pixel_color(9, light.rgb(128, 255, 219))
+        pause(200)
+        light.clear()
+    control.run_in_parallel(on_run_in_parallel2)
+    
 input.button_a.on_event(ButtonEvent.CLICK, on_button_a_click)
 
 def on_gesture_tilt_right():
     keyboard.key("d", KeyboardKeyEvent.PRESS)
+    keyboard.clear_all_keys()
 input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
 def on_button_b_click():
     keyboard.key("i", KeyboardKeyEvent.PRESS)
-    music.pew_pew.play()
-    light.show_ring("""red red red red red red red red red red""")
-    light.clear()
+    keyboard.clear_all_keys()
+    
+    def on_run_in_parallel3():
+        music.pew_pew.play()
+        
+        def on_run_in_parallel4():
+            light.set_brightness(10)
+            light.set_pixel_color(4, col_list[0])
+            pause(75)
+            light.set_brightness(10)
+            light.set_pixel_color(3, light.rgb(0, 119, 182))
+            pause(75)
+            light.set_brightness(10)
+            light.set_pixel_color(2, light.rgb(72, 191, 227))
+            pause(75)
+            light.set_brightness(10)
+            light.set_pixel_color(1, light.rgb(0, 180, 216))
+            pause(75)
+            light.set_brightness(10)
+            light.set_pixel_color(0, light.rgb(128, 255, 219))
+            pause(200)
+            light.clear()
+        control.run_in_parallel(on_run_in_parallel4)
+        
+    control.run_in_parallel(on_run_in_parallel3)
+    
 input.button_b.on_event(ButtonEvent.CLICK, on_button_b_click)
 
 def on_switch_moved_left():
     keyboard.key("p", KeyboardKeyEvent.PRESS)
 input.on_switch_moved(SwitchDirection.LEFT, on_switch_moved_left)
+
+col_list: List[number] = []
+col_list = [light.rgb(55, 6, 23)]
 
 def on_forever():
     if input.buttons_ab.is_pressed():
--- main.ts
+++ main.ts
@@ -12,33 +12,12 @@ input.buttonA.onEvent(ButtonEvent.Click, function () {
     music.jumpUp.play()
     control.runInParallel(function () {
         light.setBrightness(10)
-        light.setPixelColor(4, light.rgb(3, 4, 94))
-        pause(75)
-        light.setBrightness(10)
-        light.setPixelColor(3, light.rgb(0, 119, 182))
-        pause(75)
-        light.setBrightness(10)
-        light.setPixelColor(2, light.rgb(0, 180, 216))
-        pause(75)
-        light.setBrightness(10)
-        light.setPixelColor(1, light.rgb(80, 187, 227))
-        pause(75)
-        light.setBrightness(10)
-        light.setPixelColor(0, light.rgb(202, 240, 248))
-        pause(200)
-        light.clear()
-    })
-    control.runInParallel(function () {
-        light.setPixelColor(5, light.rgb(3, 4, 94))
-        pause(75)
-        light.setPixelColor(6, light.rgb(0, 119, 182))
-        pause(75)
-        light.setPixelColor(7, light.rgb(0, 180, 216))
-        pause(75)
-        light.setPixelColor(8, light.rgb(80, 187, 227))
-        pause(75)
-        light.setPixelColor(9, light.rgb(202, 240, 248))
-        pause(200)
+        for(let i =4; i>=0; i--){        
+            light.setPixelColor(i, jump_colors[4-i]);
+            light.setPixelColor(9-i, jump_colors[4-i]);
+            pause(75)
+        }
+pause(125)
         light.clear()
     })
 })
@@ -51,15 +30,21 @@ input.buttonB.onEvent(ButtonEvent.Click, function () {
     keyboard.clearAllKeys()
     music.pewPew.play()
     control.runInParallel(function () {
-        light.showRing(
-        `red red red red red red red red red red`
-        )
+        light.setBrightness(10)
+        for(let j =4; j>=0; j--){        
+            light.setPixelColor(j, shoot_colors[4-j]);
+            light.setPixelColor(9-j, shoot_colors[4-j]);
+            pause(75)
+        }
+pause(125)
         light.clear()
     })
 })
 input.onSwitchMoved(SwitchDirection.Left, function () {
     keyboard.key("p", KeyboardKeyEvent.Press)
 })
+let jump_colors = [light.rgb(3, 4, 94), light.rgb(0, 119, 182), light.rgb(0, 180, 216), light.rgb(80, 187, 227), light.rgb(202, 240, 248)]
+let shoot_colors = [light.rgb(157, 2, 8), light.rgb(208, 0, 0), light.rgb(232, 93, 4), light.rgb(244, 140, 6), light.rgb(255, 186, 8)]
 forever(function () {
     if (input.buttonsAB.isPressed()) {
         keyboard.key("j", KeyboardKeyEvent.Press)
