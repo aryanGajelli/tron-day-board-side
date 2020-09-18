0ed479a5d05ff4f3f399fc75f02cdd3c4f3aebad
AbhiGP23 <71521870+AbhiGP23@users.noreply.github.com>
2020-09-18 15:02:06 -0400

    Added Mappings

--- main.ts
+++ main.ts
@@ -1,11 +1,33 @@
-input.buttonB.onEvent(ButtonEvent.Click, function () {
+input.onSwitchMoved(SwitchDirection.Right, function () {
+    keyboard.key("[", KeyboardKeyEvent.Press)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    keyboard.key("a", KeyboardKeyEvent.Press)
+})
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    keyboard.key("q", KeyboardKeyEvent.Press)
+    music.jumpUp.play()
     light.showRing(
-    `black black black black black red red red red red`
+    `yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow`
     )
+    light.clear()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    keyboard.key("d", KeyboardKeyEvent.Press)
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    keyboard.key("i", KeyboardKeyEvent.Press)
     music.pewPew.play()
+    light.showRing(
+    `red red red red red red red red red red`
+    )
+    light.clear()
+})
+input.onSwitchMoved(SwitchDirection.Left, function () {
+    keyboard.key("p", KeyboardKeyEvent.Press)
 })
 forever(function () {
-    light.showRing(
-    `black black black black black black black black black black`
-    )
+    if (input.buttonsAB.isPressed()) {
+        keyboard.key("j", KeyboardKeyEvent.Press)
+    }
 })
