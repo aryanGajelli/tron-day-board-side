4235600acdd263a57e94b43a43cd4c052d223204
Aryan Gajelli <45264359+aryanGajelli@users.noreply.github.com>
2020-09-18 15:12:35 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+def on_switch_moved_right():
+    keyboard.key("[", KeyboardKeyEvent.PRESS)
+input.on_switch_moved(SwitchDirection.RIGHT, on_switch_moved_right)
+
+def on_gesture_tilt_left():
+    keyboard.key("a", KeyboardKeyEvent.PRESS)
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
 def on_button_a_click():
-    light.show_ring("""red red red red red black black black black black""")
+    keyboard.key("q", KeyboardKeyEvent.PRESS)
     music.jump_up.play()
+    light.show_ring("""yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow""")
+    light.clear()
 input.button_a.on_event(ButtonEvent.CLICK, on_button_a_click)
 
+def on_gesture_tilt_right():
+    keyboard.key("d", KeyboardKeyEvent.PRESS)
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
 def on_button_b_click():
-    light.show_ring("""black black black black black red red red red red""")
+    keyboard.key("i", KeyboardKeyEvent.PRESS)
     music.pew_pew.play()
+    light.show_ring("""red red red red red red red red red red""")
+    light.clear()
 input.button_b.on_event(ButtonEvent.CLICK, on_button_b_click)
 
+def on_switch_moved_left():
+    keyboard.key("p", KeyboardKeyEvent.PRESS)
+input.on_switch_moved(SwitchDirection.LEFT, on_switch_moved_left)
+
 def on_forever():
-    light.show_ring("""black black black black black black black black black black""")
+    if input.buttons_ab.is_pressed():
+        keyboard.key("j", KeyboardKeyEvent.PRESS)
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 input.onSwitchMoved(SwitchDirection.Right, function () {
     keyboard.key("[", KeyboardKeyEvent.Press)
-    keyboard.clearAllKeys()
 })
 input.onGesture(Gesture.TiltLeft, function () {
     keyboard.key("a", KeyboardKeyEvent.Press)
-    keyboard.clearAllKeys()
 })
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     keyboard.key("q", KeyboardKeyEvent.Press)
-    keyboard.clearAllKeys()
     music.jumpUp.play()
     light.showRing(
     `yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow`
@@ -17,11 +14,9 @@ input.buttonA.onEvent(ButtonEvent.Click, function () {
 })
 input.onGesture(Gesture.TiltRight, function () {
     keyboard.key("d", KeyboardKeyEvent.Press)
-    keyboard.clearAllKeys()
 })
 input.buttonB.onEvent(ButtonEvent.Click, function () {
-    keyboard.key("i", KeyboardKeyEvent.Down)
-    keyboard.clearAllKeys()
+    keyboard.key("i", KeyboardKeyEvent.Press)
     music.pewPew.play()
     light.showRing(
     `red red red red red red red red red red`
@@ -30,11 +25,9 @@ input.buttonB.onEvent(ButtonEvent.Click, function () {
 })
 input.onSwitchMoved(SwitchDirection.Left, function () {
     keyboard.key("p", KeyboardKeyEvent.Press)
-    keyboard.clearAllKeys()
 })
 forever(function () {
     if (input.buttonsAB.isPressed()) {
         keyboard.key("j", KeyboardKeyEvent.Press)
-        keyboard.clearAllKeys()
     }
 })
