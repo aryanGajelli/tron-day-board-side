4bfbeeaf6856b79bbb4396702f8e76846cbb13bd
AbhiGP23 <71521870+AbhiGP23@users.noreply.github.com>
2020-09-18 15:11:22 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 input.onSwitchMoved(SwitchDirection.Right, function () {
     keyboard.key("[", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
 })
 input.onGesture(Gesture.TiltLeft, function () {
     keyboard.key("a", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
 })
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     keyboard.key("q", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
     music.jumpUp.play()
     light.showRing(
     `yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow`
@@ -14,9 +17,11 @@ input.buttonA.onEvent(ButtonEvent.Click, function () {
 })
 input.onGesture(Gesture.TiltRight, function () {
     keyboard.key("d", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
 })
 input.buttonB.onEvent(ButtonEvent.Click, function () {
-    keyboard.key("i", KeyboardKeyEvent.Press)
+    keyboard.key("i", KeyboardKeyEvent.Down)
+    keyboard.clearAllKeys()
     music.pewPew.play()
     light.showRing(
     `red red red red red red red red red red`
@@ -25,9 +30,11 @@ input.buttonB.onEvent(ButtonEvent.Click, function () {
 })
 input.onSwitchMoved(SwitchDirection.Left, function () {
     keyboard.key("p", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
 })
 forever(function () {
     if (input.buttonsAB.isPressed()) {
         keyboard.key("j", KeyboardKeyEvent.Press)
+        keyboard.clearAllKeys()
     }
 })
